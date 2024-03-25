c768354a50f16a2f451bf81ac4c30103363e32a6
HongChengZ <moonwolfzhang@gmail.com>
2024-03-25 13:03:06 -0400

    i

---
+++ RollingBall/Assets/ASSETS/Scripts/CoinController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinController : MonoBehaviour
+{
+    public int CoinsNeeded;
+    public string CoinsCollisionTag;
+    public GameObject ObjectToToggle;
+    public Text countText;
+    public Text winText;
+    public bool canWin; 
+    [SerializeField]
+    private int count;
+    public AudioSource AS; 
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = ""; 
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            other.gameObject.SetActive(false);
+            AS.Play(); 
+            count = count + 1;
+            SetCountText ();
+        }
+
+        if (other.gameObject.tag == "Flag" && canWin)
+        {
+            winText.text = "YOU WIN!";
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Coins collected: " + count.ToString();
+
+        if (count >= CoinsNeeded)
+        {
+            winText.text = "You've collected all the coins!";
+            ObjectToToggle.SetActive(!ObjectToToggle.activeInHierarchy);
+            canWin = true; 
+        }
+    }
+}
